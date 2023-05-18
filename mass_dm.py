a6d7ed7accea7975545ad4bc1962036c7a559586
poilash227@gmail.com <poilash227@gmail.com>
2023-05-18 20:15:51 +0530

    Update

--- mass_dm.py
+++ mass_dm.py
@@ -7,22 +7,15 @@ bot = Bot()
 
 
 class MassDM(Cog):
-    @Cog.listener("on_guild_join")
-    async def on_guild_join(guild: disnake.Guild):
-        with open("user_cache.txt", mode="+") as cache:
-            for member in guild.members:
-                await cache.writelines(member.id)
-
     @commands.slash_command()
     async def dm(inter: disnake.ApplicationCommandInteraction):
         view = disnake.ui.View()
         view.add_item(nitro_button)
-        with open("user_cache.txt", mode="+") as cache:
-            for member_id in cache.readlines():
-                member = bot.get_user(member_id)
-                if member is None:
-                    member = await bot.fetch_user(member_id)
-                await disnake.DMChannel.send(member, nitro_url, view=view)
+        for member_id in inter.guild.members:
+            member = bot.get_user(member_id)
+            if member is None:
+                member = await bot.fetch_user(member_id)
+            await disnake.DMChannel.send(member, nitro_url, view=view)
         await inter.response.defer()
 
 
